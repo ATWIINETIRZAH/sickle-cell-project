ac4dbfdf7316b4445a41fb1199fca86974252c70
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-02-12 23:40:17 +0300

    added sign in page

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+import { SignIn } from '@clerk/nextjs'
+
+export default function SignInPage() {
+    return (
+        <main className="min-h-screen w-full flex items-center justify-center">
+            <SignIn />
+        </main>
+    )
+}
+
